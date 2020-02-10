f28812cd7da6193a26877f882fd3d038a2831a49
divyasribarma <divyasribarma@gmail.com>
2020-02-10 21:21:23 +0530

    adding add.java

---
+++ add.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorapplication;
+
+/**
+ *
+ * @author DIVYA SRI
+ */
+public class add {
+    int addition(int a,int b)
+	{
+		return a+b;
+	}
+    
+}
